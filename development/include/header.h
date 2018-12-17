2f72c10e8f371b1876deaa3edfbd3dc2f685926d
Bastien NISOLE <bnisole@ingima.com>
2018-12-17 17:22:39 +0100

    list processus + envoie input

--- development/include/header.h
+++ development/include/header.h
@@ -1,13 +1,16 @@
 
 #ifndef HEADERS_H_INCLUDED
 #define HEADERS_H_INCLUDED
+
 #define SUCCESS 1
 #define ERROR 0
+#define WINVER 0x0500
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <psapi.h>
+
 #include "mouseEvent.h"
 #include "windowsEvent.h"
 #include "inputEvent.h"
--- development/source/windowsEvent.c
+++ development/source/windowsEvent.c
@@ -1,7 +1,9 @@
 #include "../include/header.h"
 
 int listAllWindows(){
+    Sleep(5000);
     HWND handle;
+    int i = 0;
     const int nChars = 256;
     char buff [256];
     handle = GetForegroundWindow();
@@ -10,13 +12,24 @@ int listAllWindows(){
     }
     while( ( handle = GetNextWindow( handle, GW_HWNDNEXT ) ) != 0){
         if ( GetWindowText( handle, buff, nChars ) > 0 
-            && strstr( buff, "Default IME" ) == NULL 
-            && strstr( buff, "Progression" ) == NULL 
-            && strstr( buff, "MSCTFIME UI" ) == NULL ){
-            printf( "Process : %s \n", buff );
-        }
-        
+        && strstr( buff, "Default IME" ) == NULL 
+        && strstr( buff, "Progression" ) == NULL 
+        && strstr( buff, "MSCTFIME UI" ) == NULL ){
+            printf( "Process %lu : %s \n", GetWindowThreadProcessId( handle, NULL ), buff );
+            i++;
+            if( GetWindowThreadProcessId( handle, NULL ) == (long unsigned int)74624){               
+                handle = FindWindowEx(handle, 0, "Edit", 0);
+                printf( "Process %lu : %s \n", GetWindowThreadProcessId( handle, NULL ), buff );
+                Sleep(100);
+                if(!PostMessage(handle, WM_KEYDOWN, VK_NUMPAD4, 1))
+                    printf("%lu\n",GetLastError());
+                //Sleep(100);
+                printf("Je passe\n");
+                break;
+            }
+        } 
     }
+    printf("Je fini\n");
     return SUCCESS;
 }
 
